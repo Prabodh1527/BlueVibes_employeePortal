db2c9ef3d9f2e2af7d9ee3eeb3961b8bb2ac9856
Prabodh1527 <gprabodhchandra@gmail.com>
2026-02-02 20:28:51 +0530

    Update DBConnection.java

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -6,9 +6,15 @@ public class DBConnection {
     public static Connection getConnection() {
         Connection con = null;
         try {
-            String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12813830?useSSL=false&serverTimezone=UTC";
-            String user = "sql12813830";
-            String pass = "7aU9wdUTF5";
+            String url =
+                "jdbc:mysql://shuttle.proxy.rlwy.net:56882/railway"
+              + "?useSSL=true"
+              + "&requireSSL=true"
+              + "&verifyServerCertificate=false"
+              + "&serverTimezone=UTC";
+
+            String user = "root";
+            String pass = "OZKiDIwjorSGALvAVhAbgmIKUCeNrVzV";
 
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pass);
