ee7da03f943fcde473dc8430ac0a04a446161f0d
Gudimella Prabodh Chandra <gprabodhchandra@gmail.com>
2026-02-15 00:50:09 +0530

    Update DBConnection.java

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,4 +1,3 @@
-import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -10,39 +9,24 @@ public class DBConnection {
 
             String databaseUrl = System.getenv("DATABASE_URL");
 
-            System.out.println("DATABASE_URL = " + databaseUrl);
+            System.out.println("=== DEBUG DATABASE_URL ===");
+            System.out.println(databaseUrl);
+            System.out.println("=== END DEBUG ===");
 
             if (databaseUrl == null) {
                 throw new RuntimeException("DATABASE_URL is NULL");
             }
 
-            URI dbUri = new URI(databaseUrl);
-
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-
-            String jdbcUrl = "jdbc:postgresql://"
-                    + dbUri.getHost()
-                    + ":"
-                    + dbUri.getPort()
-                    + dbUri.getPath()
-                    + "?sslmode=require";
-
-            System.out.println("JDBC URL = " + jdbcUrl);
-
             Class.forName("org.postgresql.Driver");
 
-            Connection con = DriverManager.getConnection(jdbcUrl, username, password);
+            Connection con = DriverManager.getConnection(databaseUrl);
 
-            System.out.println("Connected Successfully");
+            System.out.println("CONNECTED SUCCESSFULLY");
 
             return con;
 
         } catch (Exception e) {
-
-            System.out.println("DATABASE CONNECTION FAILED");
-            e.printStackTrace();   // THIS IS IMPORTANT
-
+            e.printStackTrace();
             return null;
         }
     }
