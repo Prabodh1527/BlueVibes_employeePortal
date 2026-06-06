2a35930f869b116d17674bc87961877ff28fc907
Gudimella Prabodh Chandra <gprabodhchandra@gmail.com>
2026-06-07 00:57:01 +0530

    Update ForgotPasswordServlet.java

--- src/main/java/ForgotPasswordServlet.java
+++ src/main/java/ForgotPasswordServlet.java
@@ -14,7 +14,7 @@ public class ForgotPasswordServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String email = request.getParameter("userEmail");
         System.out.println("FORGOT PASSWORD HIT");
-        System.out.println("EMAIL = + email");
+        System.out.println("EMAIL = " + email);
         String tempPassword = generateRandomPassword(8);
 
         try (Connection con = DBConnection.getConnection()) {
@@ -52,9 +52,9 @@ public class ForgotPasswordServlet extends HttpServlet {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
 
         props.put("mail.smtp.connectiontimeout", "10000");
         props.put("mail.smtp.timeout", "10000");
